3239163a69ceb4316518c523a2967afc95268868
Matus Cuper <matus.cuper@gmail.com>
2017-09-03 19:09:36 +0200

    Rename script, refactor code

--- chessboard.py
+++ chessboard.py
@@ -3,17 +3,23 @@
 # vypise pole v tvare sachovnice 
 def print_chessboard(board):
 	for i in range(64):
-
 		print(board[i], end=" ")
 		if i%8 == 7:
 			print("")
 
+def print_chessboard(board, height, width):
+	for i in range(height * width):
+		print(board[i], end=" ")
+		if i%width == (width - 1):
+			print("")
 
 
 # v tomto pripade sa jedna o cistu sachovnicu
 sachovnica = [0 for i in range(64)]
+
 #print_chessboard(sachovnica)
 print(" ")
+
 # v tomto pripade sa jedna o poziciach na sachovnici 
 sach = [i for i in range(64)]
 #print_chessboard(sach)
@@ -21,12 +27,7 @@ sach = [i for i in range(64)]
 sachovnica[9] = 9
 #print_chessboard(sachovnica)
 
-def print_chessboard2(board, height, width):
-	for i in range(height * width):
-		print(board[i], end=" ")
-		if i%width == (width - 1):
-			print("")
 x = 5
 t = 5
-print_chessboard2(sachovnica, x, t)
+print_chessboard(sachovnica, x, t)
 
