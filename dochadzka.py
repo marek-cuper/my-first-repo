e3486b0db809ed620ad859420f7dc1bf18879d9a
Matus Cuper <matus.cuper@gmail.com>
2017-11-01 13:08:11 +0100

    Fix bugs, reformat script, rename variables

--- dochadzka.py
+++ dochadzka.py
@@ -1,14 +1,23 @@
 #!/usr/bin/python
 
-cas = (time.strftime("%d/%m/%Y"))
-print("Zapis svoju aktivitu v tento den.")
-print("Bol si v posilke?")
-posilka_1 = raw_input("0-nie, 1-ano, 2-na plavarni, 3-ina aktivita:  ")
-kroky_1 = raw_input("A zadaj pocet krokov, ktore si dnes vykonal: ")
 
-def aktivita(posilka_2, kroky_2):
-	f = open(l_dochadzka, "a+")
-	f.write(cas + (" aktivita: ") + posilka_2 + (", Pocet krokov: ") + kroky_2 + "\n")
+from time import strftime
+
+
+dochadzka_file = "posilka.txt"
+
+
+def aktivita(posilka, kroky):
+	f = open(dochadzka_file, "a+")
+	f.write(strftime("%d/%m/%Y") + (" aktivita: ") + posilka + (", Pocet krokov: ") + kroky + "\n")
 	f.close
 
-aktivita(posilka_1, kroky_1)
+
+print("Zapis svoju aktivitu v tento den.")
+print("Bol si v posilke?")
+
+posilka = raw_input("0-nie, 1-ano, 2-na plavarni, 3-ina aktivita:  ")
+kroky = raw_input("A zadaj pocet krokov, ktore si dnes vykonal: ")
+
+aktivita(posilka, kroky)
+
