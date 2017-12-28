5b0672831aa3c374591511e7799e11a997ea49e0
marek-cuper <marek.cuper.mc@gmail.com>
2017-12-28 20:52:19 +0100

    fix bugs

--- zoradenie.py
+++ zoradenie.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-pocet_cisiel = ""
+
 vstup = ""
 list = []
 list_cisiel1 = []
@@ -17,17 +17,21 @@ while vstup != "koniec":
 		if vstup != "koniec":
 			print("Oops! Toto nie je cislo, skuste znova alebo ukoncite pomocou prikazu koniec")
 
-if vstup == "koniec":
-	pocet_cisel = len(list_cisiel1)
-	list_cisiel1 = list_cisiel2
-	for i in range(pocet_cisiel):
-		porovnavane_cislo = list_cisiel2[i]
-		for u in range((pocet_cisiel)-1):
-			if porovnavane_cislo >= list_cisiel2[u]:
-			elif u == ((pocet_cisel) -1):
+print(list_cisiel1)
+for i in range(len(list_cisiel1)):
+	porovnavane_cislo = list_cisiel1[i]
+	for u in range(len(list_cisiel1)):
+		print(list)
+		if porovnavane_cislo < list_cisiel1[u]:
+			if porovnavane_cislo == list_cisiel1[len(list_cisiel1)-1]:
 				list.append(porovnavane_cislo)
-				break
+			#	list_cisiel1.remove(porovnavane_cislo)
+		elif porovnavane_cislo > list_cisiel1[u]:
+			if list_cisiel1[u] == list_cisiel1[len(list_cisiel1)-1]:
+				list.append(list_cisiel1[u])
+			#	list_cisiel1.remove(list_cisiel1[u])
 			else:
-				 porovnavane_cislo = list_cisiel2[u]
-	print(list)
+				porovnavane_cislo = list_cisiel1[u]
+print(list)
+
 
