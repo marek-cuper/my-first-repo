61458aa7c89c787d25b7fb596a34b70d1786d82d
marek-cuper <marek.cuper.mc@gmail.com>
2018-01-26 20:21:30 +0100

    fix bug

--- najmensie_cislo.py
+++ najmensie_cislo.py
@@ -1,25 +1,25 @@
 #!/usr/bin/python
 
-vstup = [8,5,6,7,4,12,10,3,9,18]
-list = []
+vstup = [1,8,9,10,6]
+vysledok = " "
 print(vstup)
 
 porovnavatel = vstup[0]
 for u in range(len(vstup)):
 	if porovnavatel < vstup[u]:
 		if porovnavatel == vstup[-1]:
-			list.append(porovnavatel)
+			vysledok = porovnavatel
 	elif porovnavatel > vstup[u]:
 		if vstup[u] == vstup[-1]:
-			list.append(porovnavatel)
+			vysledok = vstup[u]
 
 		else:
 			porovnavatel = vstup[u]
 
-if len(list) == 0:
-	list.append(porovnavatel)
+if vysledok == " ":
+	vysledok = porovnavatel
 
-print(list)
+print(vysledok)
 
 
 
