89917c091cf100f800ef03d2eeb1c80416b4049f
marek-cuper <marek.cuper.mc@gmail.com>
2018-01-26 20:42:02 +0100

    move code to function

--- najmensie_cislo.py
+++ najmensie_cislo.py
@@ -1,26 +1,24 @@
 #!/usr/bin/python
 
-vstup = [1,8,9,10,6]
-vysledok = " "
-print(vstup)
 
-porovnavatel = vstup[0]
-for u in range(len(vstup)):
-	if porovnavatel < vstup[u]:
-		if porovnavatel == vstup[-1]:
-			vysledok = porovnavatel
-	elif porovnavatel > vstup[u]:
-		if vstup[u] == vstup[-1]:
-			vysledok = vstup[u]
+def smallest(vstup):
+	vysledok = " "
+	porovnavatel = vstup[0]
+	for u in range(len(vstup)):
+		if porovnavatel < vstup[u]:
+			if porovnavatel == vstup[-1]:
+				vysledok = porovnavatel
+		elif porovnavatel > vstup[u]:
+			if vstup[u] == vstup[-1]:
+				vysledok = vstup[u]
+			else:
+				porovnavatel = vstup[u]
 
-		else:
-			porovnavatel = vstup[u]
+	if vysledok == " ":
+		vysledok = porovnavatel
 
-if vysledok == " ":
-	vysledok = porovnavatel
-
-print(vysledok)
+	return(vysledok)
 
 
-
+print(smallest([5,4,8,3]))
 
